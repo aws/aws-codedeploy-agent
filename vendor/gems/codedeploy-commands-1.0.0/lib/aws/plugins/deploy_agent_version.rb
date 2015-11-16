0938fe16aac1c1eed27a3825bf0bba366ef43b6c
Binbin Lu <binbinlu@amazon.com>
2015-11-16 14:55:25 -0800

    move loading version tracking file into initialize block

--- vendor/gems/codedeploy-commands-1.0.0/lib/aws/plugins/deploy_agent_version.rb
+++ vendor/gems/codedeploy-commands-1.0.0/lib/aws/plugins/deploy_agent_version.rb
@@ -2,16 +2,19 @@ module Aws
   module Plugins
     class DeployAgentVersion < Seahorse::Client::Plugin
       class Handler < Seahorse::Client::Handler
-        def call(context)
+        def initialize(handler = nil)
+          @handler = handler
           file_path = File.join(ProcessManager::Config.config[:version_dir], '.version')
           if File.exist?(file_path)
-      	    agent_version = File.read(file_path).chomp.split(': ').last
+            @agent_version ||= File.read(file_path).split(': ').last.strip
           else 
-            agent_version = "UNKNOWN_VERSION"
+            @agent_version ||= "UNKNOWN_VERSION"
             log(:warn, "Version tracking file doesn't exist in directory #{file_path}")
           end
+        end
 
-          context.http_request.headers['x-amz-codedeploy-agent-version'] = agent_version
+        def call(context)
+          context.http_request.headers['x-amz-codedeploy-agent-version'] = @agent_version
           @handler.call(context)
         end
         
