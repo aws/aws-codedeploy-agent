1850735757e47ef0451f311b131402af975b75ab
Binbin Lu <binbinlu@amazon.com>
2015-11-16 22:50:29 -0800

    remove version directory from config file

--- vendor/gems/codedeploy-commands-1.0.0/lib/aws/plugins/deploy_agent_version.rb
+++ vendor/gems/codedeploy-commands-1.0.0/lib/aws/plugins/deploy_agent_version.rb
@@ -4,18 +4,27 @@ module Aws
       class Handler < Seahorse::Client::Handler
         def initialize(handler = nil)
           @handler = handler
-          file_path = File.join(ProcessManager::Config.config[:version_dir], '.version')
+          file_path = get_version_tracking_file
           if File.exist?(file_path)
             @agent_version ||= File.read(file_path).split(': ').last.strip
           else 
             @agent_version ||= "UNKNOWN_VERSION"
-            log(:warn, "Version tracking file doesn't exist in directory #{file_path}")
+            log(:warn, "Version tracking file either does not exist or cannot be read.")
           end
         end
 
         def call(context)
           context.http_request.headers['x-amz-codedeploy-agent-version'] = @agent_version
           @handler.call(context)
+        end
+
+        def get_version_tracking_file
+          version_dir = ProcessManager::Config.config[:root_dir]
+          if version_dir.eql? 'Amazon/CodeDeploy'
+            file_path = File.join(version_dir, '.version')
+          else 
+            file_path = File.join(version_dir, '..', '.version')
+          end
         end
         
         private
