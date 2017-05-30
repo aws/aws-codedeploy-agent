4981d5174d558397cb93e2689a48e850015e1146
Asaf Erlich <aeerlich@amazon.com>
2017-05-30 16:04:25 -0700

    Fix CodeDeploy agent integ tests to use a different bucket per account - unfortunately I discovered bucket names are global so no one in the world can ever use the same bucket name
    cr https://cr.amazon.com/r/7193920/

--- features/step_definitions/step_constants.rb
+++ features/step_definitions/step_constants.rb
@@ -1,8 +1,11 @@
 class StepConstants
   CODEDEPLOY_TEST_PREFIX = "codedeploy-agent-integ-test-" unless defined?(CODEDEPLOY_TEST_PREFIX)
   IS_WINDOWS = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/) unless defined?(IS_WINDOWS)
-  APP_BUNDLE_BUCKET_SUFFIX = IS_WINDOWS ? '-windows' : '-linux' unless defined?(APP_BUNDLE_BUCKET_SUFFIX)
-  APP_BUNDLE_BUCKET = "#{CODEDEPLOY_TEST_PREFIX}bucket#{APP_BUNDLE_BUCKET_SUFFIX}" unless defined?(APP_BUNDLE_BUCKET)
+  APP_BUNDLE_BUCKET_SUFFIX = IS_WINDOWS ? '-win' : '-linux' unless defined?(APP_BUNDLE_BUCKET_SUFFIX)
+
+  #So I can grab the aws access key so the bucket is unique per account - unfortunately bucket names are global
+  AwsCredentials.instance.configure
+  APP_BUNDLE_BUCKET = "#{CODEDEPLOY_TEST_PREFIX}bucket-#{ENV['AWS_ACCESS_KEY_ID']}#{APP_BUNDLE_BUCKET_SUFFIX}".downcase unless defined?(APP_BUNDLE_BUCKET)
   APP_BUNDLE_KEY = 'app_bundle.zip' unless defined?(APP_BUNDLE_KEY)
   SAMPLE_APP_BUNDLE_DIRECTORY = IS_WINDOWS ? 'sample_app_bundle_windows' : 'sample_app_bundle_linux' unless defined?(SAMPLE_APP_BUNDLE_DIRECTORY)
   SAMPLE_APP_BUNDLE_FULL_PATH = "#{Dir.pwd}/features/resources/#{StepConstants::SAMPLE_APP_BUNDLE_DIRECTORY}" unless defined? SAMPLE_APP_BUNDLE_FULL_PATH
