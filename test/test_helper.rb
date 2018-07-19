fff3a0c60a6e1068685fbdaa55577e5abf0c4fee
Trung Lê <trung.le@ruby-journal.com>
2018-07-20 01:39:27 +1000

    Refactor the way instance_agent_helper is required

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,4 +18,4 @@ require 'base64'
 
 # require local test helpers. If you need a helper write,
 # keep this pattern or you'll be punished hard
-require 'instance_agent_helper'
+require_relative './helpers/instance_agent_helper'
