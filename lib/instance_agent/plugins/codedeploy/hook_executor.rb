a268d1bbcb91a37f25614b66d7a2f6e0d6a70cd5
Katyal <rohkat@amazon.com>
2018-07-25 10:56:36 -0700

    Fix the hook execution windows tests
     Prior to this change, The tests for windows were failing.
    
     This change add the necessary require in the hook_executor file.
    
    cr https://code.amazon.com/reviews/CR-2695179

--- lib/instance_agent/plugins/codedeploy/hook_executor.rb
+++ lib/instance_agent/plugins/codedeploy/hook_executor.rb
@@ -4,7 +4,7 @@ require 'json'
 require 'fileutils'
 
 require 'instance_agent/plugins/codedeploy/application_specification/application_specification'
-
+require 'instance_agent/platform/thread_joiner'
 module InstanceAgent
   module Plugins
     module CodeDeployPlugin
