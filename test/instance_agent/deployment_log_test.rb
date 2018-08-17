0506971dfa5111342cd9a3c28d0052db1758d687
Trung Lê <trung.le@ruby-journal.com>
2018-08-17 18:17:27 +1000

    Fetch the last line

--- test/instance_agent/deployment_log_test.rb
+++ test/instance_agent/deployment_log_test.rb
@@ -15,7 +15,7 @@ class InstanceAgentDeploymentLogTest < InstanceAgentTestCase
     should 'prints log output with local time' do
       Timecop.freeze(Time.local(2008, 9, 1, 12, 0, 0)) do
         InstanceAgent::DeploymentLog.instance.log("Use local time")
-        assert_equal("[2008-09-01 12:00:00.000] Use local time\n", `cat #{@log_file}`)
+        assert_equal("[2008-09-01 12:00:00.000] Use local time\n", `tail -n 1 #{@log_file}`)
       end
     end
   end
@@ -28,7 +28,7 @@ class InstanceAgentDeploymentLogTest < InstanceAgentTestCase
     should 'prints log output with local time' do
       Timecop.freeze(Time.local(2018, 9, 1, 15, 0, 0)) do
         InstanceAgent::DeploymentLog.instance.log("Use local time")
-        assert_equal("[2018-09-01 15:00:00.000] Use local time\n", `cat #{@log_file}`)
+        assert_equal("[2018-09-01 15:00:00.000] Use local time\n", `tail -n 1 #{@log_file}`)
       end
     end
   end
@@ -41,7 +41,7 @@ class InstanceAgentDeploymentLogTest < InstanceAgentTestCase
     should 'prints log output with UTC ISO8601 time' do
       Timecop.freeze(Time.local(2018, 9, 1, 14, 0, 0)) do
         InstanceAgent::DeploymentLog.instance.log("Use UTC ISO8601 formatted time")
-        assert_equal("[2018-09-01T04:00:00.000Z] Use UTC ISO8601 formatted time\n", `cat #{@log_file}`)
+        assert_equal("[2018-09-01T04:00:00.000Z] Use UTC ISO8601 formatted time\n", `tail -n 1 #{@log_file}`)
       end
     end
   end
