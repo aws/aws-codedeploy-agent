1940670b21c6c2dfa0daac5a967f86e59c2f014c
Chris Dibble <cdibble@amazon.com>
2021-02-22 15:59:27 +0000

    CDVSA-185 making appspec filename configurable and support yml and yaml extensions
    cr https://code.amazon.com/reviews/CR-45225568

--- lib/instance_agent/plugins/codedeploy/command_executor.rb
+++ lib/instance_agent/plugins/codedeploy/command_executor.rb
@@ -156,7 +156,7 @@ module InstanceAgent
                 :deployment_root_dir => deployment_root_dir(deployment_spec),
                 :last_successful_deployment_dir => last_successful_deployment_dir(deployment_spec.deployment_group_id),
                 :most_recent_deployment_dir => most_recent_deployment_dir(deployment_spec.deployment_group_id),
-                :app_spec_path => app_spec_path)
+                :app_spec_path => deployment_spec.app_spec_path)
                 script_log.concat_log(hook_command.execute)
               end
               script_log.log
@@ -185,7 +185,7 @@ module InstanceAgent
           return unless File.exist? last_successful_install_file_location
           File.open last_successful_install_file_location do |f|
             return f.read.chomp
-          end 
+          end
         end
 
         private
@@ -194,29 +194,30 @@ module InstanceAgent
           return unless File.exist? most_recent_install_file_location
           File.open most_recent_install_file_location do |f|
             return f.read.chomp
-          end 
+          end
         end
 
         private
         def default_app_spec(deployment_spec)
-          default_app_spec_location = File.join(archive_root_dir(deployment_spec), app_spec_path)
-          log(:debug, "Checking for app spec in #{default_app_spec_location}")
-          validate_app_spec_hooks(ApplicationSpecification::ApplicationSpecification.parse(File.read(default_app_spec_location)), deployment_spec.all_possible_lifecycle_events)
+          app_spec_location = app_spec_real_path(deployment_spec)
+          validate_app_spec_hooks(app_spec_location, deployment_spec.all_possible_lifecycle_events)
         end
 
         private
-        def validate_app_spec_hooks(app_spec, all_possible_lifecycle_events)
+        def validate_app_spec_hooks(app_spec_location, all_possible_lifecycle_events)
+          app_spec = ApplicationSpecification::ApplicationSpecification.parse(File.read(app_spec_location))
+          app_spec_filename = File.basename(app_spec_location)
           unless all_possible_lifecycle_events.nil?
             app_spec_hooks_plus_hooks_from_mapping = app_spec.hooks.keys.to_set.merge(@hook_mapping.keys).to_a
             unless app_spec_hooks_plus_hooks_from_mapping.to_set.subset?(all_possible_lifecycle_events.to_set)
               unknown_lifecycle_events = app_spec_hooks_plus_hooks_from_mapping - all_possible_lifecycle_events
-              raise ArgumentError.new("appspec.yml file contains unknown lifecycle events: #{unknown_lifecycle_events}")
+              raise ArgumentError.new("#{app_spec_filename} file contains unknown lifecycle events: #{unknown_lifecycle_events}")
             end
 
             app_spec_hooks_plus_hooks_from_default_mapping = app_spec.hooks.keys.to_set.merge(InstanceAgent::Plugins::CodeDeployPlugin::CommandPoller::DEFAULT_HOOK_MAPPING.keys).to_a
             custom_hooks_not_found_in_appspec = custom_lifecycle_events(all_possible_lifecycle_events) - app_spec_hooks_plus_hooks_from_default_mapping
             unless (custom_hooks_not_found_in_appspec).empty?
-              raise ArgumentError.new("You specified a lifecycle event which is not a default one and doesn't exist in your appspec.yml file: #{custom_hooks_not_found_in_appspec.join(',')}")
+              raise ArgumentError.new("You specified a lifecycle event which is not a default one and doesn't exist in your #{app_spec_filename} file: #{custom_hooks_not_found_in_appspec.join(',')}")
             end
           end
 
@@ -239,7 +240,6 @@ module InstanceAgent
 
         private
         def download_from_s3(deployment_spec, bucket, key, version, etag)
-
           log(:info, "Downloading artifact bundle from bucket '#{bucket}' and key '#{key}', version '#{version}', etag '#{etag}'")
           options = s3_options()
           s3 = Aws::S3::Client.new(options)
@@ -298,7 +298,7 @@ module InstanceAgent
           proxy_uri = nil
           if InstanceAgent::Config.config[:proxy_uri]
             proxy_uri = URI(InstanceAgent::Config.config[:proxy_uri])
-          end          
+          end
           options[:http_proxy] = proxy_uri
 
           if InstanceAgent::Config.config[:log_aws_wire]
@@ -310,10 +310,10 @@ module InstanceAgent
                 64 * 1024 * 1024)
             options[:http_wire_trace] = true
           end
-          
-          options          
-        end  
-        
+
+          options
+        end
+
         private
         def download_from_github(deployment_spec, account, repo, commit, anonymous, token)
 
@@ -426,7 +426,7 @@ module InstanceAgent
 
           # If the top level of the archive is a directory that contains an appspec,
           # strip that before giving up
-          if ((archive_root_files.size == 1) && 
+          if ((archive_root_files.size == 1) &&
               File.directory?(File.join(dst, archive_root_files[0])) &&
               Dir.entries(File.join(dst, archive_root_files[0])).grep(/appspec/i).any?)
             log(:info, "Stripping leading directory from archive bundle contents.")
@@ -441,7 +441,7 @@ module InstanceAgent
             FileUtils.mv(nested_archive_root, dst)
             FileUtils.rmdir(tmp_dst)
 
-            log(:debug, Dir.entries(dst).join("; "))            
+            log(:debug, Dir.entries(dst).join("; "))
           end
         end
 
@@ -457,7 +457,7 @@ module InstanceAgent
           File.open(most_recent_install_file_path(deployment_spec.deployment_group_id), 'w+') do |f|
             f.write deployment_root_dir(deployment_spec)
           end
-        end  
+        end
 
         private
         def cleanup_old_archives(deployment_spec)
@@ -469,7 +469,7 @@ module InstanceAgent
 
           full_path_deployment_archives = deployment_archives.map{ |f| File.join(ProcessManager::Config.config[:root_dir], deployment_group, f)}
           full_path_deployment_archives.delete(deployment_root_dir(deployment_spec))
-          
+
           extra = full_path_deployment_archives.size - @archives_to_retain + 1
           return unless extra > 0
 
@@ -482,7 +482,7 @@ module InstanceAgent
 
           # Absolute path takes care of relative root directories
           directories = oldest_extra.map{ |f| File.absolute_path(f) }
-          log(:debug,"Delete Files #{directories}" )
+          log(:debug, "Delete Files #{directories}")
           InstanceAgent::Platform.util.delete_dirs_command(directories)
 
         end
@@ -495,6 +495,24 @@ module InstanceAgent
         private
         def app_spec_path
           'appspec.yml'
+        end
+
+        # Checks for existence the possible extensions of the app_spec_path (.yml and .yaml)
+        private
+        def app_spec_real_path(deployment_spec)
+          app_spec_param_location = File.join(archive_root_dir(deployment_spec), deployment_spec.app_spec_path)
+          app_spec_yaml_location = File.join(archive_root_dir(deployment_spec), "appspec.yaml")
+          app_spec_yml_location = File.join(archive_root_dir(deployment_spec), "appspec.yml")
+          if File.exist? app_spec_param_location
+            log(:debug, "Using appspec file #{app_spec_param_location}")
+            app_spec_param_location
+          elsif File.exist? app_spec_yaml_location
+            log(:debug, "Using appspec file #{app_spec_yaml_location}")
+            app_spec_yaml_location
+          else
+            log(:debug, "Using appspec file #{app_spec_yml_location}")
+            app_spec_yml_location
+          end
         end
 
         private
