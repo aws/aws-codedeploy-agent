37cc2fe2ab4db10897ba9d97440d65a331735feb
Varun Rao <raovaru@amazon.com>
2021-06-28 13:43:07 -0700

    Generates lcov file for workflow actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,18 @@
+require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.single_report_path = 'coverage/lcov.info'
+end
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::LcovFormatter,
+  ]
+)
+SimpleCov.start
+
 # encoding: UTF-8
 $:.unshift "lib"
 Gem.use_paths(nil, Gem.path << "vendor")
