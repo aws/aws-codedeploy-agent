b0d4c4ff80fcefcdbc0f04029324ac8730310b22
Varun Rao <raovaru@amazon.com>
2021-06-28 13:34:06 -0700

    Trigger Workflow Actions for push and pr on master branch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,20 @@
+require 'simplecov'
+
+SimpleCov.start do
+  if ENV['CI']
+    require 'simplecov-lcov'
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      c.single_report_path = 'coverage/lcov.info'
+    end
+
+    formatter SimpleCov::Formatter::LcovFormatter
+  end
+
+  add_filter %w[version.rb initializer.rb]
+end
+
 # Encoding: UTF-8
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'bundler/setup'
